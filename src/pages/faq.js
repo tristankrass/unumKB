245adde5df6cb8958384702aaaaae2eb5200e669
Tristan Krass <tristankrass@gmail.com>
2019-06-08 12:58:08 +0300

    Faq page functionality

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -8,25 +8,39 @@ import { useStaticQuery, graphql } from "gatsby";
 const Faq = () => {
   const data = useStaticQuery(graphql`
   {
-    site {
-      siteMetadata {
-        title
-        description
+    allContentfulFaq(sort: {fields:[updatedAt], order:DESC}) {
+      nodes {
+        question
+        answer {
+          json
+        }
+        updatedAt(formatString: "MMMM Do, YYYY")
       }
     }
   }
-
 `)
+
   return (
     <Layout>
       <SEO title="FAQ" />
       <h1>FAQ</h1>
-      <h4>
-        {data.site.siteMetadata.title}
-      </h4>
-      <p>
-        {data.site.siteMetadata.description}
-      </p>
+      <section>
+        {data.allContentfulFaq.nodes.map((qa, id) => {
+          return (
+            <div key={id}>
+              <h1>
+                {qa.question}
+              </h1>
+              <p>
+                {json.content[0].content[0].value}
+              </p>
+              <p>
+                {qa.updatedAt}
+              </p>
+            </div>
+          )
+        })}
+      </section>
     </Layout>
   )
 
